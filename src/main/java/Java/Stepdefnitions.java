2f4ce9b4d59c036a36b6cd4ab1f0893d748a7c7a
Nivi1827 <nivi1827@gmail.com>
2019-12-11 10:20:28 +0530

    // sample push cmd int a.;

--- src/main/java/Java/Stepdefnitions.java
+++ src/main/java/Java/Stepdefnitions.java
@@ -9,7 +9,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Stepdefnitions {
-        // sample push cmd int a;
+        // sample push cmd int a.;
 	WebDriver driver;
 	@Given("^launch the url$")
 	public void launch()
